926cd0dce5e3ad5a7b7a0cf89d897c078af514cf
Maker <Maker>
2024-04-20 13:35:08 +0200

    Modulized Code, added readGroundMoisture() and printDHTData() methods

--- plantAutomta.c
+++ plantAutomta.c
@@ -11,30 +11,37 @@
 
 #define DHTPIN 29
 
+double* readGroundMoisture(){
+    double value = analogRead(MCP3004_BASE + AD_CHAN);
+    double* moisture = malloc(sizeof(int));
+    *moisture = (1-value/1023)*100;
+
+    return moisture;
+}
+
+void printDHTData(int* dht_data){
+    if(dht_data == NULL){
+            printf("Checksum Error\n");
+        }
+        else{
+            printf("Humidity:\t%d.%d%%\nTemperature:\t%d.%dC\n", dht_data[0], dht_data[1], dht_data[2], dht_data[3]); 
+        }
+}
+
 int main()
 {
     wiringPiSetup();
     mcp3004Setup(MCP3004_BASE, SPI_CHAN);
     
-    // while(1){
-    //     int value = analogRead(MCP3004_BASE + AD_CHAN);
-    //     printf("%d\n", value);
-    //     delay(100);
-    // }
-    int errors = 0;
     for(int i = 0 ; i < 100 ; i++){
-        
         delay(1000); //delay 2 sec for DHT "cooldown"
+        double* moisture = readGroundMoisture();
         int* dht_data = readDHT(DHTPIN);
-        //printf("%p\n", (void*)dht_data);
-        if(dht_data == NULL){
-            printf("%d: Checksum Error\n", i);
-            errors++;
-        }
-        else{
-            printf("%d: Humidity: %d.%d%%\nTemperature: %d.%dC\n", i, dht_data[0], dht_data[1], dht_data[2], dht_data[3]); 
-        }
+
+        printf("Dirt Moisture:\t%.1f%%\n", *moisture); //im just gonna pull some numbers out my ass and say above 70% is the optimal watering level
+        printDHTData(dht_data);
+        
+        printf("------------------------\n");
     }
-    printf("%d/100 errors", errors);
     return 0;
 }
